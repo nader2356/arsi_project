b3869dad1f0ff0793e6c5ce7047832f1f3696193
nader2356 <naderabdessaied8@gmail.com>
2023-11-09 19:03:07 +0100

    add some change in code

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { SigninComponent } from '../components/signin/signin.component';
-import { SignupComponent } from '../components/signup/signup.component';
-import { RecoverPasswordComponent } from '../components/recover-password/recover-password.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { RecoverPasswordComponent } from './components/recover-password/recover-password.component';
+
 
 
 @NgModule({
