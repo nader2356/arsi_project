9f417f9bae9c039b26fa21c817487f27f36af929
nader2356 <naderabdessaied8@gmail.com>
2023-11-17 21:51:15 +0100

    fix route

--- src/app/private/private-routing.module.ts
+++ src/app/private/private-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { BaseComponent } from './components/base/base.component';
 import { UserlistComponent } from './components/userlist/userlist.component';
 import { FormationsComponent } from './components/formations/formations.component';
-
 import { EditEventComponent } from './components/events/edit-event/edit-event.component';
 import { EventsComponent } from './components/events/events.component';
 import { OpportunityComponent } from './components/opportunity/opportunity.component';
+import { ChangeInformationComponent } from './components/change-formations/change-formations.component';
 
 
 const routes: Routes = [
@@ -17,35 +17,41 @@ const routes: Routes = [
       {
         path: 'user-list',
         component: UserlistComponent,
-     },
-     {
-      path: 'formations',
-      component: FormationsComponent
-    },
-    {
-    path: 'admin',
-    component: FormationsComponent
+      },
+      {
+        path: 'formations',
+        component: FormationsComponent,
+      },
+      {
+        path: 'opportunity',
+        component: OpportunityComponent,
+      },
+      {
+        path: 'admin',
+        component: FormationsComponent,
+      },
+      {
+        path: 'events',
+        component: EventsComponent,
+      },
+      {
+        path: 'events/:id',
+        component: EditEventComponent,
+      },
+      {
+        path: 'change',
+        component: ChangeInformationComponent,
+      },
+
+      {
+        path: '**',
+        redirectTo: '',
+      },
+    ],
   },
-  {
-    path: 'opportunity',
-    component: OpportunityComponent
-   },
-  {
-   path: 'events',
-   component: EventsComponent
-  },
-  {
-   path: 'events/:id',
-   component: EditEventComponent
-  },
-   {
-    path: '**',
-    redirectTo: '',
-  },
-    ]
-  },
- 
 ];
+ 
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
