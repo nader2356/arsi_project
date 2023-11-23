1b4fd5928e0b4f31cc7b87757b6dcd1d6ae91e46
nader2356 <naderabdessaied8@gmail.com>
2023-11-23 19:26:43 +0100

    fix error interceptor

--- src/app/core/interceptor/http-interceptor.service.ts
+++ src/app/core/interceptor/http-interceptor.service.ts
@@ -1,49 +1,51 @@
 import {
-    HttpErrorResponse,
-    HttpEvent,
-    HttpHandler,
-    HttpInterceptor,
-    HttpRequest,
-    HttpResponse,
-  } from '@angular/common/http';
-  import { Injectable } from '@angular/core';
-  import { Router } from '@angular/router';
-  import { Observable, throwError } from 'rxjs';
-  import { catchError, finalize, map } from 'rxjs/operators';
-  import { AuthService } from 'src/app/auth/services/auth.service';
-  
-  @Injectable()
-  export class AppHttpInterceptor implements HttpInterceptor {
-    constructor(private authService: AuthService, private router: Router) {}
-  
-    intercept(
-      req: HttpRequest<any>,
-      next: HttpHandler
-    ): Observable<HttpEvent<any>> {
-      console.log("interceptor 1");
-      if (this.authService.isLoggedIn) {
-        const authToken = this.authService.getToken();
-        console.log("interceptor --is connceted");
-  
-        const cloned = req.clone({
-          headers: req.headers.set('Authorization', 'Bearer ' + authToken),
-        });
-  
-        return next.handle(cloned).pipe(
-          map((event: HttpEvent<any>) => {
-            return event;
-          }),
-          catchError((error: HttpErrorResponse) => {
-            if (error.status == 401) {
-              this.authService.logout();
-              this.router.navigate(['/auth/login']);
-            }
-            return throwError(error);
-          })
-        );
-      } else {
-        console.log("interceptor --is deconected");
-        return next.handle(req).pipe(finalize(() => {}));
-      }
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, throwError } from 'rxjs';
+import { catchError, finalize, map } from 'rxjs/operators';
+import { AuthService } from 'src/app/auth/services/auth.service';
+@Injectable()
+export class AppHttpInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService, private router: Router) {}
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    
+    console.log('interceptor 1');
+
+    if (this.authService.isLoggedIn) {
+      const authToken = this.authService.getToken();
+      console.log('interceptor --is connceted');
+
+      const cloned = req.clone({
+        headers: req.headers.set('Authorization', 'Bearer ' + authToken),
+      });
+      return next.handle(cloned).pipe(
+        map((event: HttpEvent<any>) => {
+          return event;
+        }),
+        catchError((error: HttpErrorResponse) => {
+          console.log(error);
+
+          if (error.status === 401 || error.status === 403) {
+            this.authService.logout();
+            this.router.navigate(['/auth/signin']);
+          }
+          return throwError(error);
+        })
+      );
+    } else {
+      console.log('interceptor --is deconected');
+
+      return next.handle(req).pipe(finalize(() => {}));
     }
   }
+}
