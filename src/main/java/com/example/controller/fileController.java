dd4f9f05cbdea56ffb747d22cc85f164fe734ea5
nader2356 <naderabdessaied8@gmail.com>
2023-11-26 11:41:20 +0100

    fix upload image

--- src/main/java/com/example/controller/fileController.java
+++ src/main/java/com/example/controller/fileController.java
@@ -4,8 +4,12 @@ import com.example.service.fileService;
 import com.example.util.Constants;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Collections;
+
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,35 +22,32 @@ import jakarta.websocket.server.PathParam;
 @Api(tags = "files ")
 @CrossOrigin("*")
 public class fileController {
-
     private final fileService service;
 
     @PostMapping(value = "uploadImage")
-    public ResponseEntity<String> uploadImage(@PathParam("file") MultipartFile file){
-        return ResponseEntity.ok( service.uploadImage(file));
+    public ResponseEntity<Object> uploadImage(@PathParam("file") MultipartFile file){
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                Collections.singletonMap("file", service.uploadImage(file)));
     }
 
     @GetMapping("img/{filename:.+}")
     public ResponseEntity<Resource> serveImage(@PathVariable String filename) {
-
-
         Resource resource = service.serveFile(filename);
         return   ResponseEntity.ok()
-        		.header(HttpHeaders.CONTENT_TYPE, "application/pdf")
+                .header(HttpHeaders.CONTENT_TYPE, "image/jpeg") // Modify the content type as needed
                 .body(resource);
     }
     @PostMapping(value = "uploadPDF")
-    public ResponseEntity<String> uploadPDF(@PathParam("file") MultipartFile file){
-        return ResponseEntity.ok(service.uploadPDF(file));
+    public ResponseEntity<Object> uploadPDF(@PathParam("file") MultipartFile file){
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                Collections.singletonMap("file", service.uploadPDF(file)));
     }
 
     @GetMapping("PDF/{filename:.+}")
     public ResponseEntity<Resource> servePDF(@PathVariable String filename) {
-
-
         Resource resource = service.serveFile(filename);
         return   ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_TYPE, "application/pdf") // Modify the content type as needed
+                .header(HttpHeaders.CONTENT_TYPE, "application/pdf")
                 .body(resource);
     }
 }
